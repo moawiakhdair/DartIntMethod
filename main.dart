7ca83dbe42f175280e48da638427533cfb792e75
moawiakhdair <136732516+moawiakhdair@users.noreply.github.com>
2025-04-21 12:54:03 +0300

    first commit

---
+++ main.dart
@@ -0,0 +1,20 @@
+void main() {
+
+ String myname = "moawiakhdair";
+
+ int myage = 24;
+
+ double myhighest = -171.4;
+
+ print(myname[0].toUpperCase());
+ 
+ print(myhighest.ceil());
+  print(myhighest.floor());
+   print(myhighest.round());
+    print(myhighest.isNegative);
+
+
+    print(myhighest.abs());
+
+
+}
